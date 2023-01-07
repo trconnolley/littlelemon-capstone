389d80c5819b2195e11a8dd9132661845795e66d
trconnolley <trconnolley@gmail.com>
2023-01-07 14:38:13 -0600

    added basic semantic structure, meta tags, open graph protocol, start of Nav content

---
+++ src/components/Nav.js
@@ -0,0 +1,16 @@
+
+
+export default function Nav() {
+    return (
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Menu</li>
+                <li>Reservations</li>
+                <li>Order Online</li>
+                <li>Login</li>
+            </ul>
+        </nav>
+    )
+}
